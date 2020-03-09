7cad4f076aeeb5b47ba5acf3217aee222205df6b
Andika Yudantara <andikayudantara98@gmail.com>
2020-03-09 18:49:49 +0700

    improve

--- main.py
+++ main.py
@@ -16,32 +16,34 @@ h=cap.get(4)
 frameArea=h*w
 areaTH=frameArea/400
 
+
 #Lines
-line_up=int(2*(h/5))
-line_down=int(3*(h/5))
+line_up=int(h*(3/5))
+line_down=int(h*(2/5))
 
-up_limit=int(1*(h/5))
-down_limit=int(4*(h/5))
+line_up_limit=int(h*(1/5))
+line_down_limit=int(h*(4/5))
 
-print("Red line y:",str(line_down))
-print("Blue line y:",str(line_up))
 line_down_color=(255,0,0)
-line_up_color=(255,0,255)
+line_up_color=(0,0,255)
+
 pt1 =  [0, line_down]
 pt2 =  [w, line_down]
 pts_L1 = np.array([pt1,pt2], np.int32)
 pts_L1 = pts_L1.reshape((-1,1,2))
+
 pt3 =  [0, line_up]
 pt4 =  [w, line_up]
 pts_L2 = np.array([pt3,pt4], np.int32)
 pts_L2 = pts_L2.reshape((-1,1,2))
 
-pt5 =  [0, up_limit]
-pt6 =  [w, up_limit]
+pt5 =  [0, line_up_limit]
+pt6 =  [w, line_up_limit]
 pts_L3 = np.array([pt5,pt6], np.int32)
 pts_L3 = pts_L3.reshape((-1,1,2))
-pt7 =  [0, down_limit]
-pt8 =  [w, down_limit]
+
+pt7 =  [0, line_down_limit]
+pt8 =  [w, line_down_limit]
 pts_L4 = np.array([pt7,pt8], np.int32)
 pts_L4 = pts_L4.reshape((-1,1,2))
 
@@ -51,8 +53,7 @@ fgbg=cv2.createBackgroundSubtractorMOG2(detectShadows=True)
 #Kernals
 kernalOp = np.ones((3,3),np.uint8)
 kernalOp2 = np.ones((5,5),np.uint8)
-kernalCl = np.ones((11,11),np.uint)
-
+kernalCl = np.ones((11,11),np.uint8)
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 cars = []
@@ -82,11 +83,12 @@ while(cap.isOpened()):
 
 
         #Find Contours
-        _, countours0,hierarchy=cv2.findContours(mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+        countours0,hierarchy=cv2.findContours(mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
         for cnt in countours0:
             area=cv2.contourArea(cnt)
-            print(area)
             if area>areaTH:
+                
+                
                 ####Tracking######
                 m=cv2.moments(cnt)
                 cx=int(m['m10']/m['m00'])
@@ -94,23 +96,23 @@ while(cap.isOpened()):
                 x,y,w,h=cv2.boundingRect(cnt)
 
                 new=True
-                if cy in range(up_limit,down_limit):
+                if cy in range(line_up_limit,line_down_limit):
                     for i in cars:
                         if abs(x - i.getX()) <= w and abs(y - i.getY()) <= h:
                             new = False
                             i.updateCoords(cx, cy)
 
-                            if i.going_UP(line_down,line_up)==True:
+                            if i.going_UP(line_up,line_down)==True:
                                 cnt_up+=1
-                                print("ID:",i.getId(),'crossed going up at', time.strftime("%c"))
-                            elif i.going_DOWN(line_down,line_up)==True:
+#                                print("ID:",i.getId(),'crossed going up at', time.strftime("%c"))
+                            elif i.going_DOWN(line_up,line_down)==True:
                                 cnt_down+=1
-                                print("ID:", i.getId(), 'crossed going up at', time.strftime("%c"))
+#                                print("ID:", i.getId(), 'crossed going down at', time.strftime("%c"))
                             break
                         if i.getState()=='1':
-                            if i.getDir()=='down'and i.getY()>down_limit:
+                            if i.getDir()=='down'and i.getY()>line_down_limit:
                                 i.setDone()
-                            elif i.getDir()=='up'and i.getY()<up_limit:
+                            elif i.getDir()=='up'and i.getY()<line_up_limit:
                                 i.setDone()
                         if i.timedOut():
                             index=cars.index(i)
@@ -142,6 +144,9 @@ while(cap.isOpened()):
         cv2.putText(frame, str_down, (10, 90), font, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
         cv2.putText(frame, str_down, (10, 90), font, 0.5, (255, 0, 0), 1, cv2.LINE_AA)
         cv2.imshow('Frame',frame)
+        cv2.waitKey(5)
+   
+
 
         if cv2.waitKey(1)&0xff==ord('q'):
             break
